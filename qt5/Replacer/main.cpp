973b450c8e313c7529942799ad71ab7802f1ff3a
peter <peter@petermail.de>
2018-05-03 19:18:21 +0200

    changed some inexpressive variable names

--- qt5/Replacer/main.cpp
+++ qt5/Replacer/main.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication app(argc, argv);
+    MainWindow main_window;
+    main_window.show();
 
-    return a.exec();
+    return app.exec();
 }
