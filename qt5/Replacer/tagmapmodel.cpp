fd0c8746959b077188caa779aaa7c2569889c7a5
peter <peter@petermail.de>
2018-05-04 16:42:05 +0200

    first compiling version of readable TagMapModel class

--- qt5/Replacer/tagmapmodel.cpp
+++ qt5/Replacer/tagmapmodel.cpp
@@ -1,6 +1,35 @@
 #include "tagmapmodel.h"
+#include <QList>
 
-TagMapModel::TagMapModel()
+TagMapModel::TagMapModel(const QMap<QString, QString> &map, QObject* pParent):
+    QAbstractTableModel(pParent),
+    m_map(map)
 {
+}
 
+int TagMapModel::rowCount(const QModelIndex &parent) const
+{
+    return m_map.count();
+}
+
+int TagMapModel::columnCount(const QModelIndex &parent) const
+{
+    return 2;
+}
+
+QVariant TagMapModel::data(const QModelIndex &index, const int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    if (index.row() >= this->rowCount())
+            return QVariant();
+
+    if (Qt::DisplayPropertyRole == role || Qt::EditRole == role)
+    {
+        QList<QString> keys{m_map.uniqueKeys()};
+        return m_map[keys[index.row()]];
+    }
+    else
+        return QVariant();
 }
--- qt5/Replacer/tagmapmodel.h
+++ qt5/Replacer/tagmapmodel.h
@@ -1,11 +1,30 @@
 #ifndef TAGMAPMODEL_H
 #define TAGMAPMODEL_H
 
+#include <QAbstractTableModel>
+#include <QObject>
+#include <QMap>
 
 class TagMapModel : public QAbstractTableModel
 {
+    Q_OBJECT
+
+private:
+    // private Member
+    // --------------
+    QMap<QString, QString> m_map;
+
 public:
-    TagMapModel();
+    // declare constructor
+    TagMapModel(const QMap<QString, QString> &map, QObject* pParent = 0);
+
+    // empty destructor
+    ~TagMapModel(){}
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role) const;
+
 };
 
 #endif // TAGMAPMODEL_H
