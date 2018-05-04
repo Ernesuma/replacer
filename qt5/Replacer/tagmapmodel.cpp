1e01729cdd043d38339695530ba116540702c44e
peter <peter@petermail.de>
2018-05-04 16:41:23 +0200

    added class TagMapModel to the project

---
+++ qt5/Replacer/tagmapmodel.cpp
@@ -0,0 +1,6 @@
+#include "tagmapmodel.h"
+
+TagMapModel::TagMapModel()
+{
+
+}
---
+++ qt5/Replacer/tagmapmodel.h
@@ -0,0 +1,11 @@
+#ifndef TAGMAPMODEL_H
+#define TAGMAPMODEL_H
+
+
+class TagMapModel : public QAbstractTableModel
+{
+public:
+    TagMapModel();
+};
+
+#endif // TAGMAPMODEL_H
